55a1a7d53822865b03b0db2dc820a3fcd237fc27
ihabbenabdarhman <ihabbenabdarhman69@gmail.com>
2022-02-20 02:50:43 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let text: string;
+text = 'hi im lerning Programming';
+text = text + 'im so exsiting';
+console.log(text);
+let text1: string;
+text1 = 'نمرين';
+console.log(text1);
+let text2: string;
+text2 =
+  'كتابة برنامج يحتوي على متغيرة واحدة يقوم بي اظافة رقم 10 الى محتوى هذه المنغيرة ثم يكتب النتيجة على منصة';
+console.log(text2);
+let text3: string;
+text3 = 'مثال';
+console.log(text3);
+let text4: string;
+text4 = 'console راقم هوا 100 لكن يجب ان يظهر  110 علا منصة ';
+console.log(text4);
+let num1: number;
+num1 = 100;
+num1 = num1 + 10;
+console.log(num1);
